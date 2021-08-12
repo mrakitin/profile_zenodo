a84d8046f341fe4f3ea35ed73428674f1ae3f1e6
Maksim Rakitin <mrakitin@bnl.gov>
2021-08-12 15:58:00 -0400

    Add a search function that prints a table with `tabulate`

--- startup/00-base.py
+++ startup/00-base.py
@@ -1,10 +1,43 @@
 import os
 
 from pyzenodo3.base import Zenodo
+from tabulate import tabulate
 
 token = os.getenv("ZENODO_SANDBOX_TOKEN", None)
 zenodo_client = Zenodo(
     api_key=token, base_url="https://sandbox.zenodo.org/api/"
 )
 
-# res = zenodo_client.search("User tools for the NSLS-II Science Network")
+
+def search_records(
+    search="User tools for the NSLS-II Science Network", showindex=True
+):
+    results = zenodo_client.search(search)
+
+    data_dict = dict(
+        ids=[],
+        titles=[],
+        versions=[],
+        files=[],
+        checksums=[],
+        dates=[],
+    )
+
+    for res in results:
+        data = res.data
+        meta = data["metadata"]
+
+        data_dict["ids"].append(data["id"])
+        data_dict["titles"].append(meta["title"])
+        data_dict["versions"].append(meta["version"])
+        data_dict["files"].append(
+            "\n".join([f"{f['key']}" for f in data["files"]])
+        )
+        data_dict["checksums"].append(
+            "\n".join([f"{f['checksum']}" for f in data["files"]])
+        )
+        data_dict["dates"].append(meta["publication_date"])
+
+    print(tabulate(data_dict, headers=data_dict.keys(), showindex=showindex))
+
+    return data_dict
